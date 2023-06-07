c75daa846efaf2f8e0e5d3363bc19a3c09ccc147
= <=>
2023-06-07 13:46:53 +0200

    Wraps example code in functions

--- src/DrawingTrees/Program.fs
+++ src/DrawingTrees/Program.fs
@@ -1,8 +1,10 @@
 ﻿open TreeDesigner
 open TreeRenderer
+open System.Diagnostics
 
-TreeDesignerChecks.runAll
-TreeRendererChecks.runAll
+let runTests() =
+  TreeDesignerChecks.runAll
+  TreeRendererChecks.runAll
 
 // let tree =
 //     Node("r", [
@@ -21,13 +23,33 @@ TreeRendererChecks.runAll
 //         Node("c", []);
 //     ])
 
-let tree =
-    Node
-      ('a',
-       [Node ('a', [Node ('a', []); Node ('a', [])]); Node ('a', [Node ('a', [])]);
-        Node ('a', [])])
+// let tree =
+//     Node
+//       ('a',
+//        [Node ('a', [Node ('a', []); Node ('a', [])]); Node ('a', [Node ('a', [])]);
+//         Node ('a', [])])
 
-let designedTree = design 62.0 tree
-designedTree |> printfn "%A"
-designedTree |> render
-let _ = System.Console.ReadKey ()
+
+let renderExamples() = 
+  TreeExamples.binaryTree 3 |> design 1.0 |> render
+  TreeExamples.parallelPaths 4 3 |> design 1.0 |> render
+
+let runBenchmarks() = 
+  let iterations = 100
+
+  let treeGen h = TreeExamples.parallelPaths 4 h
+
+  seq {1..iterations} |> Seq.iter (fun _ -> treeGen 2 |> design 1.0 |> ignore)
+
+  seq {0..200} 
+    |> Seq.iter 
+      (fun height -> 
+        let start = Stopwatch.GetTimestamp()
+        let tree = treeGen height
+        seq {1..iterations} |> Seq.iter (fun _ -> design 1.0 tree |> ignore)
+        let elapsedNs = (Stopwatch.GetElapsedTime start).TotalNanoseconds / (float iterations)
+        // printfn $"Binary tree height: {height}. Design avg time: {elapsedNs} ns"
+        printfn $"{height}|{elapsedNs}"
+      )
+
+runTests()
