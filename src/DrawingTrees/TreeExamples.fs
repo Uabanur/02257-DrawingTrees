d8b648fdad4fef4d91a06bda5bcc0e1498bdac85
= <=>
2023-06-07 13:45:49 +0200

    Creates TreeExamples module

---
+++ src/DrawingTrees/TreeExamples.fs
@@ -0,0 +1,17 @@
+module TreeExamples
+open System
+
+let rnd = new Random()
+open TreeDesigner
+let singleNode =
+    Node("root", [])
+
+let rec binaryTree height = 
+    match height with 
+    | 0 -> Node(0, [])
+    | n -> Node(n, [binaryTree (n-1); binaryTree (n-1)]) 
+
+let parallelPaths paths height =
+    let height = max height 1
+    let rec pathOfLength = function | 0 -> Node(0, []) | n -> Node(n, [pathOfLength (n-1)])
+    Node(height, seq {1..paths} |> Seq.map (fun _ -> pathOfLength (height-1)) |> Seq.toList)
